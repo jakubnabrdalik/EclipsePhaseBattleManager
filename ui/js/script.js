959ad849551f904fe4448425fe7b2103b311fcbe
Tomasz Szopiński <tomasz@szopinski.net.pl>
2013-02-22 21:21:39 +0100

    [UI] JS bootstrap

--- ui/js/script.js
+++ ui/js/script.js
@@ -1,5 +1,36 @@
 (function ($) {
 	
-	console.log($);
+	var host = '',
+		urls = {
+			listAll: '/listAll',
+			list: '/list/{query}',
+			attack: '/attack',
+			update: '/update'
+		},
 	
+		updateGameState = function(gameState, callback)
+		{
+			// update table
+			// update log
+			// update ...
+			
+			callback(params);
+		},
+		askHero = function(oportunity, callback)
+		{
+			/*
+				oportunity: {
+					question: "Do something!"
+					responseKey1: 'Run!',
+					responseKey2: 'Fight!',
+					responseKey3: 'Say: Lalala!'
+				}
+			*/
+			// pokaż popup z pytaniem i odpowiedziami
+			
+			
+			
+			callback(response);
+		};
+		
 })(jQuery);
